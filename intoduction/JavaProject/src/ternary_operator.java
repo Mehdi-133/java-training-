da22709471d99cc6872721b71f351e03cee67357
Mehdi-133 <mehdikarbitou@gmail.com>
2025-05-29 02:59:01 +0100

    first commit

--- intoduction/JavaProject/src/ternary_operator.java
+++ intoduction/JavaProject/src/ternary_operator.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ternary_operator {
     public static void main(String[] args) {
 
@@ -5,23 +6,32 @@ public class ternary_operator {
 
         // variables = (conditions ) ? ifTrue : ifFalse 
          
+        Scanner scanner = new Scanner (System.in);
+
+        int number  ; 
+        System.out.print("enter an number: ");
+        number = scanner.nextInt();
+        String  evenOrOdd = (number % 2 == 0 ) ? "even" : "odd" ;
         
 
-        int number = 3 ; 
-
-        String  evenOrOdd = (number % 2 == 0 ) ? "even" : "odd" ;
-        System.out.println(evenOrOdd);
+        System.out.println("the number " + number+ " is "  +  evenOrOdd);
 
         int hours = 13;
         String timeOfDay = (hours < 12 ) ?  "A.M" : "P.M ";
         System.out.println(timeOfDay);
 
-        int income = 60000 ;
-        double taxRate = (income >= 40000) ? 0.25 : 0.15 ;
+        int  income = 60000 ;
+        System.out.print("enter your income: ");
+        income = scanner.nextInt();
+        double taxRate = (income >= 2000) ? 1.2 : 2.2 ;
         System.out.println(taxRate);
 
+        int age = 8;
+        String voiding = (age >= 18) ? "adult" : "not adult " ;
+        System.out.println(voiding);
 
 
+       scanner.close();
 
     }
 
