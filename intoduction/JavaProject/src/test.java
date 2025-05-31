13dd66515ab64bd6480ce4125d0d60b80cefedca
Mehdi-133 <mehdikarbitou@gmail.com>
2025-05-31 15:28:32 +0100

    for little bro

--- intoduction/JavaProject/src/test.java
+++ intoduction/JavaProject/src/test.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+        String name ;
+        int age ;
+        boolean pro = true;
+
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+        
+
+
+
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        if (age >= 12 ) {
+
+            System.out.println("you can sign");
+              System.out.print("are you a player: (true/false)");
+              pro = scanner.nextBoolean();
+              if (true ) {
+
+                System.out.println("your welcome");
+
+
+                
+              }
+             
+                
+            }
+            
+        
+        else{
+            System.out.println("you should be 12 first ");
+        }
+
+
+      
+
+      
+
+
+
+
         
     }
 
